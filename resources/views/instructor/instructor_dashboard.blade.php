5a6aafbd1c3bfaab4762eb0b9be7fc402eeba53f
pocari1210 <pocari.design@gmail.com>
2023-11-15 00:30:49 +0900

    マルチ認証パート1

---
+++ resources/views/instructor/instructor_dashboard.blade.php
@@ -0,0 +1,8 @@
+<html>
+<title>Instructor Dashboard </title>
+
+<body>
+  <h1>Instructor Dashboard </h1>
+</body>
+
+</html>
--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,9 @@
 
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AdminController;
+use App\Http\Controllers\InstructorController;
+use App\Http\Controllers\UserController;
 
 /*
 |--------------------------------------------------------------------------
@@ -15,17 +18,24 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+  return view('welcome');
 });
 
 Route::get('/dashboard', function () {
-    return view('dashboard');
+  return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
 Route::middleware('auth')->group(function () {
-    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
-    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
-    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+  Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+  Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+  Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
-require __DIR__.'/auth.php';
+require __DIR__ . '/auth.php';
+
+
+Route::get('/admin/dashboard', [AdminController::class, 'AdminDashboard'])
+  ->name('admin.dashboard');
+
+Route::get('/instructor/dashboard', [InstructorController::class, 'InstructorDashboard'])
+  ->name('instructor.dashboard');
