74a5af9f7d726975deff7afc2a84efa387aef958
pocari1210 <pocari.design@gmail.com>
2023-11-16 23:02:02 +0900

    AdminProfileパート2追記

--- resources/views/admin/admin_profile_view.blade.php
+++ resources/views/admin/admin_profile_view.blade.php
@@ -50,15 +50,24 @@
             </div>
           </div>
         </div>
+
         <div class="col-lg-8">
           <div class="card">
             <div class="card-body">
               <div class="row mb-3">
                 <div class="col-sm-3">
-                  <h6 class="mb-0">Full Name</h6>
+                  <h6 class="mb-0">Name</h6>
                 </div>
                 <div class="col-sm-9 text-secondary">
-                  <input type="text" class="form-control" value="John Doe" />
+                  <input type="text" name="name" class="form-control" value="{{ $profileData->name }}" />
+                </div>
+              </div>
+              <div class="row mb-3">
+                <div class="col-sm-3">
+                  <h6 class="mb-0">User Name</h6>
+                </div>
+                <div class="col-sm-9 text-secondary">
+                  <input type="text" name="username" class="form-control" value="{{ $profileData->username }}" />
                 </div>
               </div>
               <div class="row mb-3">
@@ -66,7 +75,7 @@
                   <h6 class="mb-0">Email</h6>
                 </div>
                 <div class="col-sm-9 text-secondary">
-                  <input type="text" class="form-control" value="john@example.com" />
+                  <input type="email" name="email" class="form-control" value="{{ $profileData->email }}" />
                 </div>
               </div>
               <div class="row mb-3">
@@ -74,15 +83,7 @@
                   <h6 class="mb-0">Phone</h6>
                 </div>
                 <div class="col-sm-9 text-secondary">
-                  <input type="text" class="form-control" value="(239) 816-9029" />
-                </div>
-              </div>
-              <div class="row mb-3">
-                <div class="col-sm-3">
-                  <h6 class="mb-0">Mobile</h6>
-                </div>
-                <div class="col-sm-9 text-secondary">
-                  <input type="text" class="form-control" value="(320) 380-4539" />
+                  <input type="text" name="phone" class="form-control" value="{{ $profileData->phone }}" />
                 </div>
               </div>
               <div class="row mb-3">
@@ -90,9 +91,30 @@
                   <h6 class="mb-0">Address</h6>
                 </div>
                 <div class="col-sm-9 text-secondary">
-                  <input type="text" class="form-control" value="Bay Area, San Francisco, CA" />
+                  <input type="text" name="address" class="form-control" value="{{ $profileData->address }}" />
                 </div>
               </div>
+
+              <div class="row mb-3">
+                <div class="col-sm-3">
+                  <h6 class="mb-0">Profile Image </h6>
+                </div>
+                <div class="col-sm-9 text-secondary">
+                  <input type="file" name="photo" class="form-control" />
+                </div>
+              </div>
+
+              <div class="row mb-3">
+                <div class="col-sm-3">
+                  <h6 class="mb-0"> </h6>
+                </div>
+                <div class="col-sm-9 text-secondary">
+                  <img src="{{ (!empty($profileData->photo)) ? url('storage/upload/admin_images/'.$profileData->photo) : url('storage/upload/no_image.jpg')}}" alt="Admin" class="rounded-circle p-1 bg-primary" width="80">
+                </div>
+              </div>
+
+
+
               <div class="row">
                 <div class="col-sm-3"></div>
                 <div class="col-sm-9 text-secondary">
@@ -106,6 +128,7 @@
       </div>
     </div>
   </div>
+</div>
 </div>
 
 
