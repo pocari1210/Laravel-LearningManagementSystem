9996b9a56dc01a656b5ffb1a45d2184361751ce1
pocari1210 <pocari.design@gmail.com>
2023-11-26 19:53:34 +0900

    コースの疎通(追記1)

--- resources/views/frontend/home/courses-area.blade.php
+++ resources/views/frontend/home/courses-area.blade.php
@@ -19,8 +19,16 @@ $categories = App\Models\Category::orderBy('category_name','ASC')
         <a class="nav-link" id="business-tab" data-toggle="tab" href="#business" role="tab" aria-controls="business" aria-selected="true">All</a>
       </li>
       @foreach ($categories as $category)
+
+      {{-------------------------------------------------------------
+
+        categoryのidは常に一意のため、hrefの#businessに
+        $category->idを付与する
+
+        --------------------------------------------------------------}}
+
       <li class="nav-item">
-        <a class="nav-link" id="business-tab" data-toggle="tab" href="#business" role="tab" aria-controls="business" aria-selected="false">{{ $category->category_name }}</a>
+        <a class="nav-link" id="business-tab" data-toggle="tab" href="#business{{ $category->id }}" role="tab" aria-controls="business" aria-selected="false">{{ $category->category_name }}</a>
       </li>
       @endforeach
 
@@ -37,7 +45,7 @@ $categories = App\Models\Category::orderBy('category_name','ASC')
             @foreach ($courses as $course)
 
             <div class="col-lg-4 responsive-column-half">
-              <div class="card card-item card-preview" data-tooltip-content="#tooltip_content_1">
+              <div class="card card-item card-preview" data-tooltip-content="#tooltip_content_1{{ $course->id }}">
                 <div class="card-image">
                   <a href="course-details.html" class="d-block">
                     <img class="card-img-top lazy" src="{{ asset($course->course_image) }}" data-src="images/img8.jpg" alt="Card image cap">
@@ -50,10 +58,9 @@ $categories = App\Models\Category::orderBy('category_name','ASC')
 
                   <div class="course-badge-labels">
 
-                    @if ($course->bestseller == 1)s
+                    @if ($course->bestseller == 1)
                     <div class="course-badge">Bestseller</div>
                     @else
-
                     @endif
 
                     @if ($course->highestrated == 1)
@@ -72,7 +79,7 @@ $categories = App\Models\Category::orderBy('category_name','ASC')
                 <div class="card-body">
                   <h6 class="ribbon ribbon-blue-bg fs-14 mb-3">{{ $course->label }}</h6>
                   <h5 class="card-title"><a href="course-details.html">{{ $course->course_name }}</a></h5>
-                  <p class="card-text"><a href="teacher-detail.html">{{ $course['user']['name'] }}</a></p>
+                  <p class="card-text"><a href=" ">{{ $course['user']['name'] }}</a></p>
                   <div class="rating-wrap d-flex align-items-center py-2">
                     <div class="review-stars">
                       <span class="rating-number">4.4</span>
@@ -101,19 +108,34 @@ $categories = App\Models\Category::orderBy('category_name','ASC')
 
           </div><!-- end row -->
         </div><!-- end tab-pane -->
-        <div class="tab-pane fade" id="design" role="tabpanel" aria-labelledby="design-tab">
+
+        @foreach ($categories as $category)
+        <div class="tab-pane fade" id="business{{ $category->id }}" role="tabpanel" aria-labelledby="business-tab">
           <div class="row">
+
+            @php
+            $catwiseCourse = App\Models\Course::where('category_id',$category->id)
+            ->where('status',1)->orderBy('id','DESC')->get();
+            @endphp
+
+            @forelse ($catwiseCourse as $course)
             <div class="col-lg-4 responsive-column-half">
               <div class="card card-item card-preview" data-tooltip-content="#tooltip_content_2">
                 <div class="card-image">
-                  <a href="course-details.html" class="d-block">
-                    <img class="card-img-top lazy" src="images/img-loading.png" data-src="images/img11.jpg" alt="Card image cap">
-                  </a>
+                  <img class="card-img-top lazy" src="{{ asset($course->course_image) }}" data-src="images/img8.jpg" alt="Card image cap">
                 </div><!-- end card-image -->
                 <div class="card-body">
-                  <h6 class="ribbon ribbon-blue-bg fs-14 mb-3">Beginner</h6>
-                  <h5 class="card-title"><a href="course-details.html">Ultimate Adobe Photoshop Training: From Beginner to Pro</a></h5>
-                  <p class="card-text"><a href="teacher-detail.html">Jose Portilla</a></p>
+                  <h6 class="ribbon ribbon-blue-bg fs-14 mb-3">{{ $course->label }}</h6>
+                  <h5 class="card-title"><a href="course-details.html">{{ $course->course_name }}</a></h5>
+
+                  {{------------------------------------
+
+                    リレーションを行ったuserモデルにアクセスし、
+                    インストラクタ名を表示
+                  
+                  --------------------------------------}}
+                  <p class="card-text"><a href=" ">{{ $course['user']['name'] }}</a></p>
+
                   <div class="rating-wrap d-flex align-items-center py-2">
                     <div class="review-stars">
                       <span class="rating-number">4.4</span>
@@ -126,172 +148,44 @@ $categories = App\Models\Category::orderBy('category_name','ASC')
                     <span class="rating-total pl-1">(20,230)</span>
                   </div><!-- end rating-wrap -->
                   <div class="d-flex justify-content-between align-items-center">
-                    <p class="card-price text-black font-weight-bold">129.99</p>
+
+                    {{--------------------------------------------------
+
+                    @ifディレクティブで
+                    discount_priceがNullだった場合の表示と
+                    登録を行っていた時の表示を行う
+                    
+                  ----------------------------------------------------}}
+
+                    @if ($course->discount_price == NULL)
+                    <p class="card-price text-black font-weight-bold">${{ $course->selling_price }} </p>
+                    @else
+                    <p class="card-price text-black font-weight-bold">${{ $course->discount_price }} <span class="before-price font-weight-medium">${{ $course->selling_price }}</span></p>
+                    @endif
+
                     <div class="icon-element icon-element-sm shadow-sm cursor-pointer" title="Add to Wishlist"><i class="la la-heart-o"></i></div>
                   </div>
                 </div><!-- end card-body -->
               </div><!-- end card -->
             </div><!-- end col-lg-4 -->
-            <div class="col-lg-4 responsive-column-half">
-              <div class="card card-item card-preview" data-tooltip-content="#tooltip_content_2">
-                <div class="card-image">
-                  <a href="course-details.html" class="d-block">
-                    <img class="card-img-top lazy" src="images/img-loading.png" data-src="images/img12.jpg" alt="Card image cap">
-                  </a>
-                  <div class="course-badge-labels">
-                    <div class="course-badge green">Free</div>
-                  </div>
-                </div><!-- end card-image -->
-                <div class="card-body">
-                  <h6 class="ribbon ribbon-blue-bg fs-14 mb-3">Beginner</h6>
-                  <h5 class="card-title"><a href="course-details.html">Ultimate Adobe Photoshop Training: From Beginner to Pro</a></h5>
-                  <p class="card-text"><a href="teacher-detail.html">Jose Portilla</a></p>
-                  <div class="rating-wrap d-flex align-items-center py-2">
-                    <div class="review-stars">
-                      <span class="rating-number">4.4</span>
-                      <span class="la la-star"></span>
-                      <span class="la la-star"></span>
-                      <span class="la la-star"></span>
-                      <span class="la la-star"></span>
-                      <span class="la la-star-o"></span>
-                    </div>
-                    <span class="rating-total pl-1">(20,230)</span>
-                  </div><!-- end rating-wrap -->
-                  <div class="d-flex justify-content-between align-items-center">
-                    <p class="card-price text-black font-weight-bold">Free</p>
-                    <div class="icon-element icon-element-sm shadow-sm cursor-pointer" title="Add to Wishlist"><i class="la la-heart-o"></i></div>
-                  </div>
-                </div><!-- end card-body -->
-              </div><!-- end card -->
-            </div><!-- end col-lg-4 -->
-            <div class="col-lg-4 responsive-column-half">
-              <div class="card card-item card-preview" data-tooltip-content="#tooltip_content_2">
-                <div class="card-image">
-                  <a href="course-details.html" class="d-block">
-                    <img class="card-img-top lazy" src="images/img-loading.png" data-src="images/img13.jpg" alt="Card image cap">
-                  </a>
-                  <div class="course-badge-labels">
-                    <div class="course-badge sky-blue">Highest rated</div>
-                  </div>
-                </div><!-- end card-image -->
-                <div class="card-body">
-                  <h6 class="ribbon ribbon-blue-bg fs-14 mb-3">Beginner</h6>
-                  <h5 class="card-title"><a href="course-details.html">Ultimate Adobe Photoshop Training: From Beginner to Pro</a></h5>
-                  <p class="card-text"><a href="teacher-detail.html">Jose Portilla</a></p>
-                  <div class="rating-wrap d-flex align-items-center py-2">
-                    <div class="review-stars">
-                      <span class="rating-number">4.4</span>
-                      <span class="la la-star"></span>
-                      <span class="la la-star"></span>
-                      <span class="la la-star"></span>
-                      <span class="la la-star"></span>
-                      <span class="la la-star-o"></span>
-                    </div>
-                    <span class="rating-total pl-1">(20,230)</span>
-                  </div><!-- end rating-wrap -->
-                  <div class="d-flex justify-content-between align-items-center">
-                    <p class="card-price text-black font-weight-bold">129.99</p>
-                    <div class="icon-element icon-element-sm shadow-sm cursor-pointer" title="Add to Wishlist"><i class="la la-heart-o"></i></div>
-                  </div>
-                </div><!-- end card-body -->
-              </div><!-- end card -->
-            </div><!-- end col-lg-4 -->
-            <div class="col-lg-4 responsive-column-half">
-              <div class="card card-item card-preview" data-tooltip-content="#tooltip_content_2">
-                <div class="card-image">
-                  <a href="course-details.html" class="d-block">
-                    <img class="card-img-top lazy" src="images/img-loading.png" data-src="images/img8.jpg" alt="Card image cap">
-                  </a>
-                  <div class="course-badge-labels">
-                    <div class="course-badge">Bestseller</div>
-                    <div class="course-badge blue">-39%</div>
-                  </div>
-                </div><!-- end card-image -->
-                <div class="card-body">
-                  <h6 class="ribbon ribbon-blue-bg fs-14 mb-3">Beginner</h6>
-                  <h5 class="card-title"><a href="course-details.html">Ultimate Adobe Photoshop Training: From Beginner to Pro</a></h5>
-                  <p class="card-text"><a href="teacher-detail.html">Jose Portilla</a></p>
-                  <div class="rating-wrap d-flex align-items-center py-2">
-                    <div class="review-stars">
-                      <span class="rating-number">4.4</span>
-                      <span class="la la-star"></span>
-                      <span class="la la-star"></span>
-                      <span class="la la-star"></span>
-                      <span class="la la-star"></span>
-                      <span class="la la-star-o"></span>
-                    </div>
-                    <span class="rating-total pl-1">(20,230)</span>
-                  </div><!-- end rating-wrap -->
-                  <div class="d-flex justify-content-between align-items-center">
-                    <p class="card-price text-black font-weight-bold">12.99 <span class="before-price font-weight-medium">129.99</span></p>
-                    <div class="icon-element icon-element-sm shadow-sm cursor-pointer" title="Add to Wishlist"><i class="la la-heart-o"></i></div>
-                  </div>
-                </div><!-- end card-body -->
-              </div><!-- end card -->
-            </div><!-- end col-lg-4 -->
-            <div class="col-lg-4 responsive-column-half">
-              <div class="card card-item card-preview" data-tooltip-content="#tooltip_content_2">
-                <div class="card-image">
-                  <a href="course-details.html" class="d-block">
-                    <img class="card-img-top lazy" src="images/img-loading.png" data-src="images/img9.jpg" alt="Card image cap">
-                  </a>
-                  <div class="course-badge-labels">
-                    <div class="course-badge red">Featured</div>
-                  </div>
-                </div><!-- end card-image -->
-                <div class="card-body">
-                  <h6 class="ribbon ribbon-blue-bg fs-14 mb-3">Beginner</h6>
-                  <h5 class="card-title"><a href="course-details.html">Ultimate Adobe Photoshop Training: From Beginner to Pro</a></h5>
-                  <p class="card-text"><a href="teacher-detail.html">Jose Portilla</a></p>
-                  <div class="rating-wrap d-flex align-items-center py-2">
-                    <div class="review-stars">
-                      <span class="rating-number">4.4</span>
-                      <span class="la la-star"></span>
-                      <span class="la la-star"></span>
-                      <span class="la la-star"></span>
-                      <span class="la la-star"></span>
-                      <span class="la la-star-o"></span>
-                    </div>
-                    <span class="rating-total pl-1">(20,230)</span>
-                  </div><!-- end rating-wrap -->
-                  <div class="d-flex justify-content-between align-items-center">
-                    <p class="card-price text-black font-weight-bold">129.99</p>
-                    <div class="icon-element icon-element-sm shadow-sm cursor-pointer" title="Add to Wishlist"><i class="la la-heart-o"></i></div>
-                  </div>
-                </div><!-- end card-body -->
-              </div><!-- end card -->
-            </div><!-- end col-lg-4 -->
-            <div class="col-lg-4 responsive-column-half">
-              <div class="card card-item card-preview" data-tooltip-content="#tooltip_content_2">
-                <div class="card-image">
-                  <a href="course-details.html" class="d-block">
-                    <img class="card-img-top lazy" src="images/img-loading.png" data-src="images/img10.jpg" alt="Card image cap">
-                  </a>
-                </div><!-- end card-image -->
-                <div class="card-body">
-                  <h6 class="ribbon ribbon-blue-bg fs-14 mb-3">Beginner</h6>
-                  <h5 class="card-title"><a href="course-details.html">Ultimate Adobe Photoshop Training: From Beginner to Pro</a></h5>
-                  <p class="card-text"><a href="teacher-detail.html">Jose Portilla</a></p>
-                  <div class="rating-wrap d-flex align-items-center py-2">
-                    <div class="review-stars">
-                      <span class="rating-number">4.4</span>
-                      <span class="la la-star"></span>
-                      <span class="la la-star"></span>
-                      <span class="la la-star"></span>
-                      <span class="la la-star"></span>
-                      <span class="la la-star-o"></span>
-                    </div>
-                    <span class="rating-total pl-1">(20,230)</span>
-                  </div><!-- end rating-wrap -->
-                  <div class="d-flex justify-content-between align-items-center">
-                    <p class="card-price text-black font-weight-bold">129.99</p>
-                    <div class="icon-element icon-element-sm shadow-sm cursor-pointer" title="Add to Wishlist"><i class="la la-heart-o"></i></div>
-                  </div>
-                </div><!-- end card-body -->
-              </div><!-- end card -->
-            </div><!-- end col-lg-4 -->
+
+
+            {{----------------------------------------------------
+
+              @emptyディレクティブ
+              forelseでデータが空だった場合の処理を記述する
+
+              Courceの登録がなかった場合の表示を記述している
+            
+            ------------------------------------------------------}}
+
+            @empty
+            <h5 class="text-danger"> No Course Found </h5>
+            @endforelse
+
           </div><!-- end row -->
         </div><!-- end tab-pane -->
+        @endforeach
       </div><!-- end tab-content -->
       <div class="more-btn-box mt-4 text-center">
         <a href="course-grid.html" class="btn theme-btn">Browse all Courses <i class="la la-arrow-right icon ml-1"></i></a>
@@ -299,3 +193,60 @@ $categories = App\Models\Category::orderBy('category_name','ASC')
     </div><!-- end container -->
   </div><!-- end card-content-wrapper -->
 </section><!-- end courses-area -->
+
+@php
+$courseData = App\Models\Course::get();
+@endphp
+
+<!-- tooltip_templates -->
+@foreach ($courseData as $item)
+
+<div class="tooltip_templates">
+
+  {{----------------------------------------------------------
+
+    CourseモデルのID情報を取得させる
+    ※記述がない場合、表示されない
+    
+    ----------------------------------------------------------}}
+  <div id="tooltip_content_1{{ $item->id }}">
+
+    <div class="card card-item">
+      <div class="card-body">
+        <p class="card-text pb-2">By <a href="teacher-detail.html">{{ $item['user']['name'] }}</a></p>
+        <h5 class="card-title pb-1"><a href="course-details.html"> {{ $item->course_name }}</a></h5>
+        <div class="d-flex align-items-center pb-1">
+          @if ($item->bestseller == 1)
+          <h6 class="ribbon fs-14 mr-2">Bestseller</h6>
+          @else
+          <h6 class="ribbon fs-14 mr-2">New</h6>
+          @endif
+
+          <p class="text-success fs-14 font-weight-medium">Updated<span class="font-weight-bold pl-1">{{ $item->created_at->format('M d Y') }}</span></p>
+        </div>
+        <ul class="generic-list-item generic-list-item-bullet generic-list-item--bullet d-flex align-items-center fs-14">
+          <li>{{ $item->duration }} total hours</li>
+          <li>{{ $item->label }}</li>
+        </ul>
+        <p class="card-text pt-1 fs-14 lh-22">{{ $item->prerequisites }}</p>
+
+        @php
+        $goals = App\Models\Course_goal::where('course_id',$item->id)
+        ->orderBy('id','DESC')->get();
+        @endphp
+
+        <ul class="generic-list-item fs-14 py-3">
+          @foreach ($goals as $goal)
+          <li><i class="la la-check mr-1 text-black"></i> {{ $goal->goal_name }}</li>
+          @endforeach
+        </ul>
+
+        <div class="d-flex justify-content-between align-items-center">
+          <a href="#" class="btn theme-btn flex-grow-1 mr-3"><i class="la la-shopping-cart mr-1 fs-18"></i> Add to Cart</a>
+          <div class="icon-element icon-element-sm shadow-sm cursor-pointer" title="Add to Wishlist"><i class="la la-heart-o"></i></div>
+        </div>
+      </div>
+    </div><!-- end card -->
+  </div>
+</div><!-- end tooltip_templates -->
+@endforeach
