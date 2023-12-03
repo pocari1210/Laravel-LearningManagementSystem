eb36272315e606faa63985fc359936dac0156f63
pocari1210 <pocari.design@gmail.com>
2023-12-03 14:55:09 +0900

    コースをカートに追加part2

--- routes/web.php
+++ routes/web.php
@@ -257,4 +257,6 @@ Route::get('/instructor/details/{id}', [IndexController::class, 'InstructorDetai
 Route::post('/add-to-wishlist/{course_id}', [WishListController::class, 'AddToWishList']);
 
 Route::post('/cart/data/store/{id}', [CartController::class, 'AddToCart']);
+
+Route::get('/cart/data/', [CartController::class, 'CartData']);
 // FrontendのRoute : END
