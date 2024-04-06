a52862c95eab385b973cb1b9cdb9089dd53a49b6
pocari1210 <pocari.design@gmail.com>
2024-04-06 19:24:24 +0900

    couponの計算処理(その2)

--- resources/views/frontend/mycart/view_mycart.blade.php
+++ resources/views/frontend/mycart/view_mycart.blade.php
@@ -62,23 +62,10 @@
         <a href="#" class="btn theme-btn mb-2">Update Cart</a>
       </div>
     </div>
-    <div class="col-lg-4 ml-auto">
-      <div class="bg-gray p-4 rounded-rounded mt-40px">
-        <h3 class="fs-18 font-weight-bold pb-3">Cart Totals</h3>
-        <div class="divider"><span></span></div>
-        <ul class="generic-list-item pb-4">
-          <li class="d-flex align-items-center justify-content-between font-weight-semi-bold">
-            <span class="text-black">Subtotal:$</span>
-            <span id="cartSubTotal"> </span>
-          </li>
-          <li class="d-flex align-items-center justify-content-between font-weight-semi-bold">
-            <span class="text-black">Total:$</span>
-            <span id="cartSubTotal"> </span>
-          </li>
-        </ul>
-        <a href="checkout.html" class="btn theme-btn w-100">Checkout <i class="la la-arrow-right icon ml-1"></i></a>
-      </div>
+    <div class="bg-gray p-4 rounded-rounded mt-40px" id="couponCalField">
+      <a href="checkout.html" class="btn theme-btn w-100">Checkout <i class="la la-arrow-right icon ml-1"></i></a>
     </div>
+  </div>
   </div><!-- end container -->
 </section>
 <!-- ================================
