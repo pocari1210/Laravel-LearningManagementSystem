71e9b7c4bdcfbef9fa02f30825c86d43e96b6557
pocari1210 <pocari.design@gmail.com>
2024-04-06 17:57:45 +0900

    coupon(編集ページ遷移と更新処理と削除処理)

--- routes/web.php
+++ routes/web.php
@@ -164,6 +164,15 @@ Route::middleware(['auth', 'roles:admin'])->group(function () {
 
     Route::post('/admin/store/coupon', 'AdminStoreCoupon')
       ->name('admin.store.coupon');
+
+    Route::get('/admin/edit/coupon/{id}', 'AdminEditCoupon')
+      ->name('admin.edit.coupon');
+
+    Route::post('/admin/update/coupon', 'AdminUpdateCoupon')
+      ->name('admin.update.coupon');
+
+    Route::get('/admin/delete/coupon/{id}', 'AdminDeleteCoupon')
+      ->name('admin.delete.coupon');
   });
 }); // End Admin Group SideBar 
 
