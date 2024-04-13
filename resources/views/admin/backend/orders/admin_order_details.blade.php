fa874d38254b81325692ec5aabc093e070141e6e
pocari1210 <pocari.design@gmail.com>
2024-04-13 21:54:23 +0900

    DB連携(courseの詳細項目追加)

--- resources/views/admin/backend/orders/admin_order_details.blade.php
+++ resources/views/admin/backend/orders/admin_order_details.blade.php
@@ -80,67 +80,121 @@
 
         <div class="col-lg-6">
           <div class="card">
-
-            <form method="POST" action="{{ route('admin.profile.store') }}" enctype="multipart/form-data">
-              @csrf
-
-              <div class="card-body">
-
-                <div class="row mb-3">
-                  <div class="col-sm-3">
-                    <h6 class="mb-0">Total Amount </h6>
-                  </div>
-                  <div class="col-sm-9 text-secondary">
-                    <strong> ${{ $payment->total_amount }}</strong>
-                  </div>
+            <div class="card-body">
+              <div class="row mb-3">
+                <div class="col-sm-3">
+                  <h6 class="mb-0">Total Amount </h6>
                 </div>
-
-                <div class="row mb-3">
-                  <div class="col-sm-3">
-                    <h6 class="mb-0">Payment Type </h6>
-                  </div>
-                  <div class="col-sm-9 text-secondary">
-                    <strong>{{ $payment->payment_type }}</strong>
-                  </div>
-                </div>
-
-                <div class="row mb-3">
-                  <div class="col-sm-3">
-                    <h6 class="mb-0">Invoice Number </h6>
-                  </div>
-                  <div class="col-sm-9 text-secondary">
-                    <strong> {{ $payment->invoice_no }}</strong>
-                  </div>
-                </div>
-
-                <div class="row mb-3">
-                  <div class="col-sm-3">
-                    <h6 class="mb-0">Order Date </h6>
-                  </div>
-                  <div class="col-sm-9 text-secondary">
-                    <strong> {{ $payment->order_date }}</strong>
-                  </div>
-                </div>
-
-                <div class="row mb-3">
-                  <div class="col-sm-3">
-                    <h6 class="mb-0">Status </h6>
-                  </div>
-                  <div class="col-sm-9 text-secondary">
-                    @if ($payment->status == 'pending')
-                    <a href="" class="btn btn-block btn-success">Confirm Order</a>
-                    @elseif ($payment->status == 'confirm')
-                    <a href="" class="btn btn-block btn-success">Confirm Order</a>
-                    @endif
-                  </div>
+                <div class="col-sm-9 text-secondary">
+                  <strong> ${{ $payment->total_amount }}</strong>
                 </div>
               </div>
-            </form>
 
+              <div class="row mb-3">
+                <div class="col-sm-3">
+                  <h6 class="mb-0">Payment Type </h6>
+                </div>
+                <div class="col-sm-9 text-secondary">
+                  <strong>{{ $payment->payment_type }}</strong>
+                </div>
+              </div>
 
+              <div class="row mb-3">
+                <div class="col-sm-3">
+                  <h6 class="mb-0">Invoice Number </h6>
+                </div>
+                <div class="col-sm-9 text-secondary">
+                  <strong> {{ $payment->invoice_no }}</strong>
+                </div>
+              </div>
+
+              <div class="row mb-3">
+                <div class="col-sm-3">
+                  <h6 class="mb-0">Order Date </h6>
+                </div>
+                <div class="col-sm-9 text-secondary">
+                  <strong> {{ $payment->order_date }}</strong>
+                </div>
+              </div>
+
+              <div class="row mb-3">
+                <div class="col-sm-3">
+                  <h6 class="mb-0">Status </h6>
+                </div>
+                <div class="col-sm-9 text-secondary">
+                  @if ($payment->status == 'pending')
+                  <a href="" class="btn btn-block btn-success">Confirm Order</a>
+                  @elseif ($payment->status == 'confirm')
+                  <a href="" class="btn btn-block btn-success">Confirm Order</a>
+                  @endif
+                </div>
+              </div>
+
+            </div>
           </div>
         </div>
       </div>
+
+      <div class="card radius-10">
+        <div class="card-body">
+          <div class="d-flex align-items-center">
+
+            <div class="flex-grow-1 ms-3">
+              <div class="table-responsive">
+                <table class="table" style="font-weight: 600;">
+                  <tbody>
+                    <tr>
+                      <td class="col-md-1">
+                        <label>Image</label>
+                      </td>
+                      <td class="col-md-2">
+                        <label>Course Name</label>
+                      </td>
+                      <td class="col-md-2">
+                        <label>Category </label>
+                      </td>
+
+                      <td class="col-md-2">
+                        <label>Instructor</label>
+                      </td>
+                      <td class="col-md-2">
+                        <label>Price</label>
+                      </td>
+                    </tr>
+
+                    @foreach ($orderItem as $item)
+                    <tr>
+                      <td class="col-md-1">
+                        <label><img src="{{ asset($item->course->course_image) }}" alt=""
+                            style="width: 50px; height:50px;"> </label>
+                      </td>
+
+                      <td class="col-md-2">
+                        <label> {{ $item->course->course_name }} </label>
+                      </td>
+
+                      <td class="col-md-2">
+                        <label>{{ $item->course->category->category_name }}</label>
+                      </td>
+
+                      <td class="col-md-2">
+                        <label> {{ $item->instructor->name }} </label>
+                      </td>
+
+                      <td class="col-md-2">
+                        <label> ${{ $item->price }} </label>
+                      </td>
+                    </tr>
+                    @endforeach
+
+                  </tbody>
+                </table>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+
     </div>
   </div>
 </div>
