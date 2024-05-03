e2eec470288d2287fdd5296422cad368f2cb6194
pocari1210 <pocari.design@gmail.com>
2024-05-03 16:18:49 +0900

    最終ログイン履歴

--- app/Http/Middleware/Role.php
+++ app/Http/Middleware/Role.php
@@ -5,6 +5,10 @@ namespace App\Http\Middleware;
 use Closure;
 use Illuminate\Http\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Cache;
+use Carbon\Carbon;
+use App\Models\User;
 
 class Role
 {
@@ -15,6 +19,17 @@ class Role
    */
   public function handle(Request $request, Closure $next, $role): Response
   {
+
+    // ★Userのログイン状況(Active/Inactive)★
+    if (Auth::check()) {
+      $expireTime = Carbon::now()->addSeconds(30);
+
+      // Cacheデータを新規追加または上書きする
+      Cache::put('user-is-online' . Auth::user()->id, true, $expireTime);
+
+      User::where('id', Auth::user()->id)
+        ->update(['last_seen' => Carbon::now()]);
+    }
 
     // roleが一致していない場合、dashboardページにリダイレクトされる
     if ($request->user()->role !== $role) {
