73226752266647fa684ffa182b13c767cb6d9df3
pocari1210 <pocari.design@gmail.com>
2024-05-08 20:50:38 +0900

    laravel-permissionインストール

--- config/app.php
+++ config/app.php
@@ -161,6 +161,7 @@ return [
         /*
          * Package Service Providers...
          */
+        Spatie\Permission\PermissionServiceProvider::class,
 
         /*
          * Application Service Providers...
